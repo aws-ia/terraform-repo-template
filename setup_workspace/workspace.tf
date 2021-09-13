e16bf8f97ca8219a72c8177a565dfe4003161efa
Tony Vattathil <avattathil@gmail.com>
2021-09-13 15:02:28 -0700

    Add setup workspace

---
+++ setup_workspace/workspace.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+resource "random_pet" "name" {
+  prefix = "tfm-aws"
+  length = 1
+}
+
+# Generate new terraform org and workspace
+
+module "tfc_workspace" {
+  source                = "aws-ia/cloud_workspace/hashicorp"
+  version               = "0.0.1"
+  tfe_email             = var.tfe_email
+  tfe_organization      = var.tfe_organization
+  tfe_workspace         = var.tfe_workspace
+  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+  AWS_SESSION_TOKEN     = var.AWS_SESSION_TOKEN
+  working_directory     = var.working_directory
+  region                = var.region
+}
+
+
+resource "null_resource" "setup_backend_file" {
+  depends_on = [module.tfc_workspace]
+  provisioner "local-exec" {
+    command = "mv backend.hcl  ..${var.working_directory}"
+  }
+}
+
+
+resource "null_resource" "remote_init" {
+  depends_on = [null_resource.setup_backend_file]
+  provisioner "local-exec" {
+    working_dir = "..${var.working_directory}"
+    command     = "terraform init -backend-config=backend.hcl"
+  }
+}
+
+output "user_instructions" {
+  value = <<README
+# org name    = ${module.tfc_workspace.tfm-aws-org-name}
+# workspace   = ${module.tfc_workspace.tfm-aws-workspace-name}
+#
+#
+# Run these commands in order:
+  cd ..${var.working_directory}
+# Configure your tfvars file
+  AWS_SECRET_ACCESS_KEY = "*****************"
+  AWS_ACCESS_KEY_ID     = "*****************"
+  AWS_SESSION_TOKEN     = "*****************"
+  region                = "region override"
+#  Note: Use of STS Creds are highly reccommended!
+# !!!!CAUTION!!!!: Make sure your credential are secured outside version control (and follow secrets mangement bestpractices)
+#   
+   terraform apply  -var-file="$HOME/.aws/terraform.tfvars"
+README
+}
