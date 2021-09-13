54a4dc8ada1f04472078807e7ab6efeabd914b70
Tony Vattathil <avattathil@gmail.com>
2021-09-13 15:16:57 -0700

    Add gitignore

---
+++ main.tf
@@ -0,0 +1,3 @@
+# Root Module
+# No backends should be defined by default in Root Module
+# This module is intended to be called by one or more other modules and must not contain any provider blocks
